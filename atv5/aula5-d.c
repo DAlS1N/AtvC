4345d05cf4ff5ec17d38184f9ca0dbfd346a7d68
Kauã Dal Santo <dalsantokaua@gmail.com>
2026-03-23 19:34:02 -0300

    Update aula5-d.c

--- atv5/aula5-d.c
+++ atv5/aula5-d.c
@@ -7,12 +7,13 @@ int x = 10; //variável do tipo inteiro
 int *p; //ponteiro de inteiro
 
 
-p = &x;  //ponteiro recebe o endereço de x
+p2 = &x;  //ponteiro recebe o endereço de x
 
 
 printf("Valor de x: %d\n", x);
 printf("Endereco de x: %p\n", &x);
-printf("Valor apontado por p: %d\n", *p);
+printf("Valor apontado por p: %d\n", *p2);
 
+free(p2)
 
 }
