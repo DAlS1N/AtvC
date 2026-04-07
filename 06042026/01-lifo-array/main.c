9120d1a18c82e5bd5ad1853a925381c77b12d048
Kauã Dal Santo <dalsantokaua@gmail.com>
2026-04-06 21:22:08 -0300

    ajustes

---
+++ 06042026/01-lifo-array/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int pilha[5];
+    int indiceTopo = -1;
+
+    pilha[++indiceTopo] = 10;
+    pilha[++indiceTopo] = 20;
+    pilha[++indiceTopo] = 30;
+    pilha[++indiceTopo] = 40;
+    pilha[++indiceTopo] = 50;
+
+    printf("Topo atual: %d\n", pilha[indiceTopo]);
+
+    printf("Removendo: %d\n", pilha[indiceTopo--]);
+
+    printf("Novo topo: %d",pilha[indiceTopo]);
+
+    return 0;
+}
---
+++ 06042026/03-pilha-dinamica-encadeada/main.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct No
+{
+    int valor;
+    struct No *proximo;
+} No;
+
+typedef struct
+{
+    No *topo;
+    int quantidade;
+} Pilha;
+
+
+void inicializar(Pilha *p){
+    p->topo = NULL;
+    p->quantidade = 0;
+}
+
+int push(Pilha *p, int valor){
+    No *novo = (No *) malloc(sizeof(No));
+    if (novo == NULL)
+    {
+        printf("Sem memoria disponivel,libere espaço e tente novamente");
+       return 0;
+    }
+
+    novo->valor = valor;
+    novo->proximo = p->topo;
+
+    //salvou o topo
+    p->topo = novo;
+    p->quantidade++;
+    return 1;
+}
+
+int pop(Pilha *p, int *valor){
+    No *temp;
+    
+    if(p->topo == NULL){
+        printf("Pilha vazia, pulando pop");
+        return 0;
+    }
+
+    temp = p->topo;
+
+
+    *valor = temp->valor;
+    p->topo = temp->proximo;
+
+
+    free(temp);
+
+    p->quantidade--;
+
+    return 1;
+}
+
+
+int main()
+{
+
+
+    Pilha p;
+    int removido;
+
+    inicializar(&p);
+
+       push(&p,10);
+       push(&p,20);
+       push(&p,30);
+       push(&p,40);
+       push(&p,50);
+       push(&p,60);
+
+    
+       while(pop(&p,&removido)){
+           printf("Valor removido: %d\n",removido);
+       }
+
+   
+
+    return 0;
+}
