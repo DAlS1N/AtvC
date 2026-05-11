13de82466839df8deba353e9382f64c5b4299661
Kauã Dal Santo <kaua_santo@estudante.sesisenai.org.br>
2026-05-11 16:08:17 -0300

    adição do bubble sort
    pelo autor Kauã Dal Santo

--- Tarefa 6 - Busca binária/main.c
+++ Tarefa 6 - Busca binária/main.c
@@ -12,3 +12,18 @@ typedef struct
     float valor;
 } Produto;
 
+/* ---------- algoritmos de ordenação ---------- */
+ 
+
+void bubble_sort(Produto v[], int n, long *comp, long *trocas) {
+    *comp = *trocas = 0;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            (*comp)++;                              /* conta a comparação */
+            if (v[j].codigo > v[j+1].codigo) {
+                Produto tmp = v[j]; v[j] = v[j+1]; v[j+1] = tmp;
+                (*trocas)++;                        /* conta a troca */
+            }
+        }
+    }
+}
